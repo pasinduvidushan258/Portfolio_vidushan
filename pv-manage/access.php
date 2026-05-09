5e368f7f00aee956345528d7ec3d344373cdd22e
Pasindu Vidushan <pasinduvidushan258@gmail.com>
2026-05-09 15:07:14 +0530

    Secured admin panel and updated robots.txt

--- pv-manage/access.php
+++ pv-manage/access.php
@@ -44,6 +44,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login_submit'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="robots" content="noindex, nofollow">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login | Pasindu Vidushan</title>
